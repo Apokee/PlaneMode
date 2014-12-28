c0bf5d7471cbc6d22302620724c3ec9451c56d78
Dwayne Bent <dbb@dbb.io>
2014-12-28 17:14:05 -0500

    Make wrapper script a little smarter

--- build.ps1
+++ build.ps1
@@ -14,5 +14,10 @@ iex "$NugetExe install `"$PackagesConfigFile`" -OutputDirectory `"$PackagesDir`"
 	Select-String -NotMatch -Pattern "All packages listed in packages.config are already installed."
 
 # Run Cake
-iex "$CakeExe build.cake $args"
+$target = $args[0];
+if ($args.length > 1)
+{
+    $extraArgs = $args[1..($args.length-1)];
+}
+iex "$CakeExe build.cake -target=$target $extraArgs"
 exit $LASTEXITCODE
