4db75c7deea39dde9a01f369d1fcdde160bcf70e
Dwayne Bent <dbb@dbb.io>
2015-05-01 13:47:18 -0400

    Update bootstrap build script

--- build.ps1
+++ build.ps1
@@ -1,9 +1,20 @@
+Param (
+	[Parameter(Position = 0)]
+	[string]$Target,
+
+	[Parameter(ValueFromRemainingArguments = $true)]
+	[Object[]]$RemainingArgs
+)
+
 # Globals
+# TODO: The experimental flag is necessary after upgrading to VS2015 until the final version of Roslyn is available
+# This should presumably occur once VS2015 is final
+$UseExperimental	= $true
 $RootDir            = "$PSScriptRoot"
 $NugetDir           = "$RootDir/.nuget"
 $NugetExe           = "$NugetDir/NuGet.exe"
 $PackagesConfigFile = "$NugetDir/packages.config"
-$LibraryDir			= "$RootDir/Library"
+$LibraryDir         = "$RootDir/Library"
 $PackagesDir        = "$LibraryDir/NuGet"
 $CakeVersionXPath   = "//package[@id='Cake'][1]/@version"
 $CakeVersion        = (Select-Xml -Xml ([xml](Get-Content $PackagesConfigFile)) -XPath $CakeVersionXPath).Node.Value
@@ -13,6 +24,17 @@ $CakeExe            = "$PackagesDir/Cake.$CakeVersion/Cake.exe"
 iex "$NugetExe install `"$PackagesConfigFile`" -OutputDirectory `"$PackagesDir`"" |
 	Select-String -NotMatch -Pattern "All packages listed in packages.config are already installed."
 
+# Build args
+$cakeArgs = @()
+
+if ($Target) {
+	$cakeArgs += "-target=$Target"
+}
+
+if ($UseExperimental) {
+	$cakeArgs += "-experimental"
+}
+
 # Run Cake
-iex "$CakeExe $args"
+iex "$CakeExe $cakeArgs $RemainingArgs"
 exit $LASTEXITCODE
